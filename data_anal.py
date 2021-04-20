f8934e2c40a549f56e5bf0945b6ee4305f56ba2d
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2021-04-20 12:21:17 +0900

    data_anal

---
+++ data_anal.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from pandas_profiling import ProfileReport
+import chess
+
+#사용자의 경우는 절대주소로 변경 바람
+#chess_data = ProfileReport(pd.read_csv('C:/Users/dkyou/Documents/GitHub/DDa_RL/games.csv'), explorative=True)
+#chess_data.to_file("C:/Users/dkyou/Documents/GitHub/DDa_RL/output.html")
+
+chess = pd.read_csv()
